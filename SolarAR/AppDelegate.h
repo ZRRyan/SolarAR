5b2c7e024d9030aa243c9ed31f5a8bc258daf25e
zhangrui <zhangrui@qq.com>
2017-11-07 19:45:10 +0800

    修改代码源文件

---
+++ SolarAR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SolarAR
+//
+//  Created by Ryan on 2017/11/7.
+//  Copyright © 2017年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
